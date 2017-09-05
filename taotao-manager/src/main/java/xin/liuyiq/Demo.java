fc68e3acb02c97109fbb571256dd66ae57f3bcf0
tom <tom@qq.com>
2017-09-05 22:19:15 +0800

    第二个人修改

--- taotao-manager/src/main/java/xin/liuyiq/Demo.java
+++ taotao-manager/src/main/java/xin/liuyiq/Demo.java
@@ -3,6 +3,7 @@ package xin.liuyiq;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("master 第一次修改操作");
+        System.out.println("master   1111   第一次修改操作");
+        System.out.println("第二个用户 第二次修改");
     }
 }
