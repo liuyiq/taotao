e7ddbf4efd6953690b77eff57644c28c31d69bb3
tom <tom@qq.com>
2017-09-05 22:26:42 +0800

    3333

--- taotao-manager/src/main/java/xin/liuyiq/Demo.java
+++ taotao-manager/src/main/java/xin/liuyiq/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("master   1111   第一次修改操作");
-        System.out.println("第二个用户 第二次修改");
+        System.out.println("第二个用户 第二次修改    dasdasdds");
+
     }
 }
